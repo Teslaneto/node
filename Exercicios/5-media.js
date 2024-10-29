6a017611c3832401173c29ad384709acf9e4050f
Geraldo <onixblackcx@gmail.com>
2024-10-29 15:01:40 -0300

    converter graus Celsius para Fahrenheit

---
+++ Exercicios/5-media.js
@@ -0,0 +1,7 @@
+let numero1 = 6;
+let numero2 = 3;
+let numero3 = 2;
+
+const media = (numero1 + numero2 + numero3) / 3;
+
+console.log(media);
---
+++ Exercicios/6-temperatura.js
@@ -0,0 +1,9 @@
+const var1 = 1.8;
+const var2 = 32;
+
+let c = 37; // C é a temperatura em graus Celsius
+
+const resul = (c * var1) + var2;
+let f = resul; // F a temperatura em Fahrenheit
+
+console.log(f);
