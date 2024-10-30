a56ee4fb4def466663e9a5f9e202d9c6bfcd4263
Geraldo <onixblackcx@gmail.com>
2024-10-30 09:31:18 -0300

    QUAL VALOR É MAIOR QUE O OUTRO

---
+++ Exercicios/10-maior-numero.js
@@ -0,0 +1,11 @@
+//VERIFICAR QUAL VALOR É MAIOR QUE O OUTRO
+let numero1 = 11;
+let numero2 = 12;
+
+if(numero1 > numero2){
+    console.log("O primeiro valor é maior que o segundo");
+}else if(numero2 > numero1){
+    console.log("O segundo valor é maior que o primeiro");
+}else{
+    console.log("Posivelmente os valores são iguais");
+}
