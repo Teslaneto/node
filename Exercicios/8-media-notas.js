cb29f1ca959a79d5e69eb96313a62b8e5e234c34
Geraldo <onixblackcx@gmail.com>
2024-10-30 09:11:56 -0300

    media-notas

---
+++ Exercicios/8-media-notas.js
@@ -0,0 +1,17 @@
+let nota1 = 10;
+let nota2 = 10;
+let nota3 = 10;
+let media = (nota1 + nota2 + nota3) / 3;
+
+switch(media){
+    case 7:
+        console.log("O aluno estar indo bem");
+    break;
+    case 6:
+        console.log("O aluno pode melhora");
+    break;
+    case 3:
+        console.log("O aluno estar Horrivel");
+    break;
+}
+
