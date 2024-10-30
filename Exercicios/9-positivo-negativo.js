2e4bdc31d734f0c595750d077bbb54e2b871004d
Geraldo <onixblackcx@gmail.com>
2024-10-30 09:27:00 -0300

    atividades

---
+++ Exercicios/9-positivo-negativo.js
@@ -0,0 +1,12 @@
+let numero = -1; //DIGITE UM NUMERO POSITIVO OU NEGATIVO
+
+if(numero > 0){
+    console.log("Seu numero é positivo ");
+}else if(numero == 0){
+    console.log("Seu numero é Neutro");
+}else if(numero < 0){
+    console.log("Seu numero é negativo ");
+}
+
+
+
