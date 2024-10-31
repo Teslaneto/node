80d4c3c0650eb36025f1b6c6e4f893ae87b87b5b
Geraldo <onixblackcx@gmail.com>
2024-10-31 10:26:41 -0300

    array

---
+++ Exercicios/14-array.js
@@ -0,0 +1,34 @@
+const numero = [1,2,3,4,5];
+
+function logArray(elemento, index, array){
+    console.log("numeros["+ index +"]"+ elemento )
+}
+numero.forEach(logArray);
+
+
+
+
+/*numero.push(6)
+console.log("Array Inicial: ",numero)
+
+let valor = numero.pop()
+let valor2 = numero.pop()
+
+console.log("Array Pop1: ",valor)
+console.log("Array Pop2: ",valor2)
+
+console.log("Array Final: ",numero);
+
+const remove = numero.shift()
+console.log("Array Shift: ",remove)
+
+numero.unshift(1)
+numero.unshift(0)
+
+console.log("Array Resultado: ",numero);
+
+const arrayslice = numero.slice(1,4)
+console.log("Array Slice ",arrayslice)
+
+console.log("Array Resultado: ",numero);*/
+
