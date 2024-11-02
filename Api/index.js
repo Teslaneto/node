57f292de0a9c7d7aec1c204709518401963a3cfd
Geraldo <onixblackcx@gmail.com>
2024-11-02 14:51:45 -0300

    api-basica

--- Api/index.js
+++ Api/index.js
@@ -1,14 +1,30 @@
 const http = require('http');
+const url = require('url');
 
 http.createServer((request, response) =>{
-    console.log(request.method);
 
-    if(request.method === 'POST'){
-        response.write('Hello, Geraldo');
-        response.end();
-    }else if(request.method === 'GET'){
-        response.write('METODO GET');
-        response.end();
+    const parsedUrl = url.parse(request.url, true);
+    const pathName = parsedUrl.pathname;
+    const queryParams = parsedUrl.query;
+
+    if(request.method === 'GET'){
+        //PODE FAZER O TESTE COLOCANDO A BARRA '/'
+
+        if(pathName === '/'){
+            response.writeHead(404, {'content-type': 'text/plain; charset:uft-8'});
+            response.write('Pagina Nao Encotrada');
+            response.end();
+            return;
+        }else{
+            response.writeHead(200, {'content-type': 'text/plain; charset:uft-8'});
+            response.write("Seja Bem-vindo "+(queryParams.name || 'Visitante'));
+            response.end();
+        }
+        
     }
-    
-}).listen(3000)
+
+}).listen(3000);
+
+
+
+//response.write('A somar é ,'+ (Number(queryParams.num1) +  Number(queryParams.num2)) ); RETORNARA A SOMA 
